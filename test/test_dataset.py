c1aeaecaa489481ce83514d748c497ca2d04431c
Aaron D Borden <adborden@a14n.net>
2019-12-03 16:10:31 -0800

    Guard against empty array values
    Fixes "TypeError: can only join an iterable" when calling map_dataset with
    theme=None.

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -3,8 +3,12 @@ import unittest
 from dataset import Dataset, map_dataset
 
 
-class TestDataset(unittest.TestCase):
+class TestMapDataset(unittest.TestCase):
+    def setUp(self):
+        self.dataset = Dataset()
+
     def test_map_dataset(self):
+        """Given data and an empty dataset, the dataset is populated with data."""
         data = dict(
             id='bae3d41e-a06e-4d70-9c41-86bb89814fc1',
             title='Social Security Administration (SSA) Enterprise Data Inventory (EDI)',
@@ -16,8 +20,28 @@ class TestDataset(unittest.TestCase):
             type='dataset',
         )
 
-        dataset = Dataset()
+        dataset = self.dataset
+        map_dataset(dataset, data)
 
+        assert 'id' in dataset
+        assert dataset['id'] == data['id']
+
+
+    def test_map_dataset_with_no_theme(self):
+        """Given data with an empty theme and an empty dataset, the dataset is populated with data."""
+        data = dict(
+            id='bae3d41e-a06e-4d70-9c41-86bb89814fc1',
+            title='Social Security Administration (SSA) Enterprise Data Inventory (EDI)',
+            metadata_created='2019-12-03T23:31:39.018162',
+            metadata_modified='2019-12-03T23:31:39.041053',
+            author=None,
+            author_email=None,
+            state='active',
+            type='dataset',
+            theme=None,
+        )
+
+        dataset = self.dataset
         map_dataset(dataset, data)
 
         assert 'id' in dataset
